ce0dbd5f871c988f90eac8b7d18527d961af10fd
Athenkosi Magada <magadaathenkosi@gmail.com>
2024-01-01 16:41:20 +0200

    Update the UI and fixed bugs

--- chatter.view/wwwroot/js/site.js
+++ chatter.view/wwwroot/js/site.js
@@ -1,4 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var click = document.querySelector('.click');
+var profile = document.querySelector('.profile-details');
+var chats = document.querySelectorAll('.chat-blog');
 
-// Write your JavaScript code.
+click.addEventListener('click', function () {
+    profile.classList.toggle('show');
+});
+
+document.addEventListener('click', function (event) {
+    const isClickInsideClickElement = click.contains(event.target);
+    const isClickInsideProfileElement = profile.contains(event.target);
+
+    if (!isClickInsideClickElement && !isClickInsideProfileElement) {
+        profile.classList.remove('show');
+    }
+});
+
+chats.forEach(chat => {
+    chat.addEventListener('click', function () {
+        // Remove 'show' class from all chats
+        chats.forEach(otherChat => {
+            if (otherChat !== chat) {
+                otherChat.classList.remove('active');
+            }
+        });
+
+        // Toggle 'avtive' class for the clicked chat
+        chat.classList.toggle('active');
+    });
+});
